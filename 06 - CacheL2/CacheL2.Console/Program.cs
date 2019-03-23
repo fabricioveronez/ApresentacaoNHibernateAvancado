857184ad4de5101de94c6942a128d02b939614b9
Luiz Carlos Faria <luizcarlosfaria@gmail.com>
2019-03-23 19:15:31 -0300

    revisão do using no session

--- 06 - CacheL2/CacheL2.Console/Program.cs
+++ 06 - CacheL2/CacheL2.Console/Program.cs
@@ -16,19 +16,23 @@ namespace CacheL2.Console
             ISessionFactory sessionFactory = Fluently.Configure()
                          .Database(MsSqlConfiguration.MsSql2012.ConnectionString("Data Source=localhost;Initial Catalog=Northwind;User ID=sa;Password=Northwind0123")
                          .ShowSql().FormatSql())
-                         .Cache(c => c.UseSecondLevelCache().ProviderClass(typeof(NHibernate.Caches.StackExchangeRedis.RedisCacheProvider).AssemblyQualifiedName))
+                         .Cache(c => 
+                             c.UseSecondLevelCache()
+                              .ProviderClass<NHibernate.Caches.StackExchangeRedis.RedisCacheProvider>()
+                         )
                          .Mappings(m =>
                              m.FluentMappings
-                             .AddFromAssemblyOf<EmployeesMap>())
+                             .AddFromAssemblyOf<EmployeesMap>()
+                         )
                          .ExposeConfiguration(cfg => cfg.SetProperty("cache.configuration", "localhost:6379"))
                          .BuildSessionFactory();
 
-            ISession session1 = sessionFactory.OpenSession();
+            using ISession session1 = sessionFactory.OpenSession();
             Products product1 = session1.Get<Products>(1);
 
             System.Console.WriteLine(product1.ProductName);
 
-            ISession session2 = sessionFactory.OpenSession();
+            using ISession session2 = sessionFactory.OpenSession();
             Products product2 = session2.Get<Products>(1);
 
             System.Console.WriteLine(product2.ProductName);

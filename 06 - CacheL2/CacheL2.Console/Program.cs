2f03ebee8cd2789c9507e93f096b63894d056ae0
fabricioveronez <fabricioveronez@hotmail.com>
2019-09-01 11:20:42 -0300

    Incluindo o docker compose

--- 06 - CacheL2/CacheL2.Console/Program.cs
+++ 06 - CacheL2/CacheL2.Console/Program.cs
@@ -13,13 +13,13 @@ namespace CacheL2.Console
             ISessionFactory sessionFactory = Fluently.Configure()
                          .Database(MsSqlConfiguration.MsSql2012.ConnectionString("Data Source=localhost;Initial Catalog=Northwind;User ID=sa;Password=Northwind0123")
                          .ShowSql().FormatSql())
-                         //.Cache(c => 
-                         //    c.UseSecondLevelCache().ProviderClass<NHibernate.Caches.StackExchangeRedis.RedisCacheProvider>()
-                         //)
+                         .Cache(c => 
+                            c.UseSecondLevelCache().ProviderClass<NHibernate.Caches.StackExchangeRedis.RedisCacheProvider>()
+                         )
                          .Mappings(m =>
                              m.FluentMappings.AddFromAssemblyOf<EmployeesMap>()
                          )
-                         //.ExposeConfiguration(cfg => cfg.SetProperty("cache.configuration", "localhost:6379"))
+                         .ExposeConfiguration(cfg => cfg.SetProperty("cache.configuration", "localhost:6379"))
                          .BuildSessionFactory();
 
             using ISession session1 = sessionFactory.OpenSession();
